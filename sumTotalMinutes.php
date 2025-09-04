e54be991d8650a0d84b823d1fec97273dc5a3797
Yin Nyein Aye <yinnyeinaye.mysol@gmail.com>
2025-09-04 16:50:21 +0630

    addGetTotalMinutes

---
+++ sumTotalMinutes.php
@@ -0,0 +1,15 @@
+<?php
+    function getTotalMinutes($str){
+        $times = explode('-',$str);
+        foreach($times as $time) {
+            $amOrPm  = substr($time,-2);
+            $hourMinute = substr($time,0,-2);
+            list($hour,$minutes) = explode(':',$hourMinute);
+            $totalTime[] = ($hour * 60) + $minutes;
+        }
+        return $totalTime[1] - $totalTime[0];
+    }
+    
+    $result = getTotalMinutes("10:00am-12:00am");
+    var_dump($result);
+?>
